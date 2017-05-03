c3c86ea622ac0d1e53c132269229764f6a137504
Dominik Mueller <domi89@pcpool203.ipsp.uni-hohenheim.de>
2017-05-03 18:00:53 +0200

    Modified remapper.R

--- R/remapper.R
+++ R/remapper.R
@@ -8,6 +8,8 @@
 #' @param new Vector. The new values.
 #' @param na_when_missing Logical. If a value cannot be found, should \code{NA} be used or
 #' value queried for? (defaults to \code{TRUE}).
+#' @param fun A function applied to the result before returning.
+#' @param \dots Further arguments for \code{fun}.
 #' @return A vector with re-mapped values.
 #'
 #' @details The factory internally uses a \code{\link[hashmap]{hashmap}} for mapping old to new
@@ -26,7 +28,7 @@
 #' remapper(old = old, new = new, na_when_missing = FALSE)(x)
 #'
 #' @export
-remapper <- function(old, new, na_when_missing = TRUE){
+remapper <- function(old, new, na_when_missing = TRUE, fun = NULL, ...){
   map <- hashmap::hashmap(keys = old, values = new)
   function(x) {
     val <- map[[x]]
@@ -34,7 +36,10 @@ remapper <- function(old, new, na_when_missing = TRUE){
       has_keys <- map$has_keys(x)
       val[!has_keys] <- x[!has_keys]
     }
+    if (is.function(fun)) {
+      val <- lapply(X = val, FUN = fun, ...)
+      val <- unlist(x = val, recursive = FALSE, use.names = FALSE)
+    }
     val
   }
 }
-
